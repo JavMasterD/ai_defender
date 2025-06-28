38c02808247a0912759677b9a24d55e8cb7ba078
JavMasterD <108193312+JavMasterD@users.noreply.github.com>
2025-06-29 00:33:46 +0300

    Add files via upload

---
+++ samples/example.py
@@ -0,0 +1,11 @@
+from ai_defender import AIDefender
+
+defender = AIDefender()
+
+print(defender.chat("فحص الشبكة"))
+print(defender.chat("فحص التطبيقات"))
+print(defender.chat("فحص الروابط"))
+print(defender.chat("قطع الشبكة"))
+print(defender.chat("حظر التطبيقات"))
+print(defender.chat("ايقاف الهجمات"))
+print(defender.chat("فحص البورتات"))
